d57b9323c1d9fa39e9f0032cbb4d5603442f2a7f
@scarlet <memory.loverz@gmail.com>
2015-09-07 12:26:29 +0900

    add process: install apt packages

--- init/init.sh
+++ init/init.sh
@@ -3,6 +3,29 @@ echo "*** EXECUTE $0"
 
 FILES=`ls $DOTPATH/init/*.sh | grep -v 'init.sh'`
 
+pkgs='
+cifs-utils
+compizconfig-settings-manager
+git
+git-flow
+ibus-mozcsudo
+language-pack-ja
+language-pack-ja-base
+openssh-server
+samba
+screen
+subversion
+vim
+vim-athena
+vim-gnome
+vim-gtk
+xclip
+'
+
+# install packages
+sudo apt-get install -y ${pkgs}
+
+# setup
 for i in $FILES; do
     bash $i
 done
